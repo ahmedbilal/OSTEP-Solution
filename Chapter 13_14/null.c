a2d7b259d9d1e1533b5828171f8de6d2c384590e
Abhi-1301 <90414121+Abhi-1301@users.noreply.github.com>
2021-11-15 10:21:07 +0530

    Add files via upload

--- Chapter 13_14/null.c
+++ Chapter 13_14/null.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
-{
-	char * greetings = "Hello World";
+int main(){
+    int *p=NULL;
+    printf("%d\n",*p);
+    return 0;
 }
+//Segmentation Fault occur. Mean we are trying to access memory location that is not declared.
